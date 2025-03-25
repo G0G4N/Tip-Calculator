dceaa87bd1661b0054852e2bf532f5e119d8a1cf
Logan <loganbubsherry@gmail.com>
2025-03-24 21:15:52 -0500

    Replaced the "Hello Android!" text with "Tip Calculator".

--- app/src/main/java/com/g0g4n/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/g0g4n/tipcalculator/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TipCalculatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Tip Calculator",
         modifier = modifier
     )
 }
